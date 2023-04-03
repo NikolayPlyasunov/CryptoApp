4e419bffcda3d7624d259134adb29192e13a623a
Nikolay <svmusic174@gmail.com>
2023-04-03 22:35:06 +0500

    Initial commit

---
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cryptoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.adapters.CoinInfoAdapter
+import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
+import com.example.cryptoapp.pojo.CoinPriceInfo
+
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    private val binding by lazy {
+        ActivityCoinPriceListBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
+            }
+        }
+        binding.rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
+        })
+    }
+
+}
